365973104e872c38618c79c85a7dea5fae633f36
Dusza Andrea <andidusza@gmail.com>
2015-08-31 01:40:18 +0200

    UI: Map zoom level decreased. Smaller images in front page list & map info windows. No estate descriptions in front page. Number of rooms only showed if not undefined. New example advertisement : an office. Mock telephone numbers and mock person to contact removed from Contact page. International telephone number in English version.
    Model:
    Dynamic place and type options in select2 coming from json data.
    Returned to default select2 matcher.
    Some unneeded CSS rules removed.

--- index-compiled.js
+++ index-compiled.js
@@ -6,48 +6,78 @@
 
 google.maps.event.addDomListener(window, 'load', initializeMap);
 $(document).ready(function () {
-    $("#town-input-1").select2({
-        width: "300px",
-        placeholder: "Írja be a településnevet",
-        allowClear: true,
-        multiple: true,
-        maximumSelectionSize: 10,
-        data: TOWNS_LIST.map(function (city, index) {
-            return { id: index, text: city };
-        }),
-
-        formatLoadMore: 'Loading more...',
-        query: function query(q) {
-            // pageSize is number of results to show in dropdown
-            var pageSize, results;
-            pageSize = 20;
-            results = this.data.filter(function (e) {
-                return q.term === "" || e.text.toUpperCase().indexOf(q.term.toUpperCase()) === 0;
-            });
-            q.callback({
-                results: results.slice((q.page - 1) * pageSize, q.page * pageSize),
-                // retrieve more when user hits bottom
-                more: results.length >= q.page * pageSize
-            });
-        }
-    });
-
     var housesList = document.getElementById("houses-list");
 
+    var townListFromJson = [];
+    var typeListFromJson = [];
     $.getJSON("houses.json", function (json) {
+        var townSetFromJson = new Set();
+        var typeSetFromJson = new Set();
         json.forEach(function (i) {
             var li = document.createElement("li");
-            li.innerHTML = "<div class=\"frame\">\n                <a href=\"" + i.detailsPageUrl + "\">\n                <img src=\"" + i.images[0] + "\" alt=\"Img\" width=\"418px\" height=\"264px\"></a>\n                </div>\n                <h5>" + i.title + "</h5>\n                <p>" + i.description + "</p>\n                <div class=\"details\">\n                <p><b>Alapterület:</b>" + i.sizeInSquareMeters + " m<sup>2</sup></p>\n                <p><b>Szobák:</b> " + i.numberOfRooms + "</p>\n                </div>\n                <span class=\"price\">" + i.price + "</span>";
+            var roomsHtmlPart = "";
+            if (i.numberOfRooms !== undefined) {
+                roomsHtmlPart = "<p><b>Szobák: </b>" + i.numberOfRooms + "</p>";
+            }
+            li.innerHTML = "<div class=\"frame\">\n                <a href=\"" + i.detailsPageUrl + "\">\n                <img class=\"houseThumbnailInFrontpageList\" src=\"" + i.images[0] + "\" alt=\"Img\" width=\"418px\" height=\"264px\"></a>\n                </div>\n                <h5>" + i.title + "</h5>\n                <div class=\"details\">\n                <p><b>Alapterület: </b>" + i.sizeInSquareMeters + " m<sup>2</sup></p>" + roomsHtmlPart + ("</div>\n                <span class=\"price\">" + i.price + "</span>");
             housesList.appendChild(li);
+            i.locationTags.forEach(function (j) {
+                townSetFromJson.add(j);
+            });
+            typeSetFromJson.add(i.type);
+        });
+
+        townSetFromJson.forEach(function (k) {
+            townListFromJson.push(k);
+        });
+        typeSetFromJson.forEach(function (k) {
+            typeListFromJson.push(k);
+        });
+
+        var typeSelect = document.getElementById("selectEstateType");
+        var option = document.createElement("option");
+        option.innerHTML = "<option>bármilyen ingatlan</option>";
+        typeSelect.appendChild(option);
+        typeSetFromJson.forEach(function (i) {
+            var option = document.createElement("option");
+            option.innerHTML = "<option>" + i + "</option>";
+            typeSelect.appendChild(option);
+        });
+
+        $("#town-input-1").select2({
+            width: "300px",
+            placeholder: "Írja be, hol keres ingatlant",
+            allowClear: true,
+            multiple: true,
+            maximumSelectionSize: 10,
+            data: townListFromJson.map(function (city, index) {
+                return { id: index, text: city };
+            }),
+
+            formatLoadMore: 'Loading more...'
         });
     });
 });
 
+/*query: function (q) {
+    // pageSize is number of results to show in dropdown
+    var pageSize,
+        results;
+    pageSize = 20;
+    results = this.data.filter(function (e) {
+        return (q.term === "" || e.text.toUpperCase().indexOf(q.term.toUpperCase()) === 0);
+    });
+    q.callback({
+        results: results.slice((q.page - 1) * pageSize, q.page * pageSize),
+        // retrieve more when user hits bottom
+        more: results.length >= q.page * pageSize
+    });
+}*/
 function initializeMap() {
     var myCenter = new google.maps.LatLng(47.456589, 18.928657);
     var mapProp = {
         center: myCenter,
-        zoom: 11,
+        zoom: 7,
         mapTypeId: google.maps.MapTypeId.HYBRID
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
@@ -59,7 +89,7 @@ function initializeMap() {
             });
             marker.setMap(map);
 
-            var contentString = "<div class=\"mapInfoWindow\">\n                <h1 class=\"mapInfoWindowFirstHeading\">" + i.title + "</h1>\n                <div><p><a href=\"" + i.detailsPageUrl + "\">\n                <img src=\"" + i.images[0] + "\" height=\"150\" width=\"200\" alt=\"Img\"></a></p>\n                <p>" + i.price + "</p>\n                <p><a href=\"" + i.detailsPageUrl + "\">Részletek itt</a></p>\n                </div>\n                </div>";
+            var contentString = "<div class=\"mapInfoWindow\">\n                <a href=\"" + i.detailsPageUrl + "\"><h1>" + i.mostSpecificLocation + "</h1></a>\n                <div><p><a href=\"" + i.detailsPageUrl + "\">\n                <img class=\"houseThumbnailInMapInfoWindow\" src=\"" + i.images[0] + "\" alt=\"Img\"></a></p>\n                <p>" + i.price + "</p>\n                </div>\n                </div>";
 
             var infowindow = new google.maps.InfoWindow({
                 content: contentString
--- index.js
+++ index.js
@@ -4,61 +4,91 @@
 
 google.maps.event.addDomListener(window, 'load', initializeMap);
 $(document).ready(function () {
-    $("#town-input-1").select2({
-        width: "300px",
-        placeholder: "Írja be a településnevet",
-        allowClear: true,
-        multiple: true,
-        maximumSelectionSize: 10,
-        data: TOWNS_LIST.map(function (city, index) {
-            return {id: index, text: city};
-        }),
-
-        formatLoadMore: 'Loading more...',
-        query: function (q) {
-            // pageSize is number of results to show in dropdown
-            var pageSize,
-                results;
-            pageSize = 20;
-            results = this.data.filter(function (e) {
-                return (q.term === "" || e.text.toUpperCase().indexOf(q.term.toUpperCase()) === 0);
-            });
-            q.callback({
-                results: results.slice((q.page - 1) * pageSize, q.page * pageSize),
-                // retrieve more when user hits bottom
-                more: results.length >= q.page * pageSize
-            });
-        }
-    });
-
     var housesList = document.getElementById("houses-list");
 
+    var townListFromJson = [];
+    var typeListFromJson = [];
     $.getJSON("houses.json", function (json) {
+        var townSetFromJson = new Set();
+        var typeSetFromJson = new Set();
         json.forEach(i => {
             var li = document.createElement("li");
+            var roomsHtmlPart="";
+            if (i.numberOfRooms !== undefined){
+                roomsHtmlPart =  "<p><b>Szobák: </b>" + i.numberOfRooms + "</p>";
+            }
             li.innerHTML = (
                 `<div class="frame">
                 <a href="${i.detailsPageUrl}">
-                <img src="${i.images[0]}" alt="Img" width="418px" height="264px"></a>
+                <img class="houseThumbnailInFrontpageList" src="${i.images[0]}" alt="Img" width="418px" height="264px"></a>
                 </div>
                 <h5>${i.title}</h5>
-                <p>${i.description}</p>
                 <div class="details">
-                <p><b>Alapterület:</b>${i.sizeInSquareMeters} m<sup>2</sup></p>
-                <p><b>Szobák:</b> ${i.numberOfRooms}</p>
-                </div>
+                <p><b>Alapterület: </b>${i.sizeInSquareMeters} m<sup>2</sup></p>`
+                + roomsHtmlPart +
+                `</div>
                 <span class="price">${i.price}</span>`
             );
             housesList.appendChild(li);
-        })
+            (i.locationTags).forEach(j => {
+                townSetFromJson.add(j);
+            });
+            typeSetFromJson.add(i.type);
+        });
+
+        townSetFromJson.forEach( k => {
+            townListFromJson.push(k);
+        });
+        typeSetFromJson.forEach( k => {
+            typeListFromJson.push(k);
+        });
+
+        var typeSelect = document.getElementById("selectEstateType");
+        var option = document.createElement("option");
+        option.innerHTML = `<option>bármilyen ingatlan</option>`;
+        typeSelect.appendChild(option);
+        typeSetFromJson.forEach(i =>{
+            var option = document.createElement("option");
+            option.innerHTML = `<option>${i}</option>`;
+            typeSelect.appendChild(option);
+        });
+
+        $("#town-input-1").select2({
+            width: "300px",
+            placeholder: "Írja be, hol keres ingatlant",
+            allowClear: true,
+            multiple: true,
+            maximumSelectionSize: 10,
+            data: townListFromJson.map(function (city, index) {
+                return {id: index, text: city};
+            }),
+
+            formatLoadMore: 'Loading more...',
+            /*query: function (q) {
+                // pageSize is number of results to show in dropdown
+                var pageSize,
+                    results;
+                pageSize = 20;
+                results = this.data.filter(function (e) {
+                    return (q.term === "" || e.text.toUpperCase().indexOf(q.term.toUpperCase()) === 0);
+                });
+                q.callback({
+                    results: results.slice((q.page - 1) * pageSize, q.page * pageSize),
+                    // retrieve more when user hits bottom
+                    more: results.length >= q.page * pageSize
+                });
+            }*/
+        });
+
     });
+
 });
 
 function initializeMap() {
     var myCenter = new google.maps.LatLng(47.456589, 18.928657);
     var mapProp = {
         center: myCenter,
-        zoom: 11,
+        zoom: 7,
         mapTypeId: google.maps.MapTypeId.HYBRID
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
@@ -72,11 +102,10 @@ function initializeMap() {
 
             var contentString =
                 `<div class="mapInfoWindow">
-                <h1 class="mapInfoWindowFirstHeading">${i.title}</h1>
+                <a href="${i.detailsPageUrl}"><h1>${i.mostSpecificLocation}</h1></a>
                 <div><p><a href="${i.detailsPageUrl}">
-                <img src="${i.images[0]}" height="150" width="200" alt="Img"></a></p>
+                <img class="houseThumbnailInMapInfoWindow" src="${i.images[0]}" alt="Img"></a></p>
                 <p>${i.price}</p>
-                <p><a href="${i.detailsPageUrl}">Részletek itt</a></p>
                 </div>
                 </div>`;
 
