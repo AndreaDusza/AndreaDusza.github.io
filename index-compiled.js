1e1f50f5f136acbdad87f18a2655dfdbcd778b64
Dusza Andrea <andidusza@gmail.com>
2015-09-04 02:20:17 +0200

    search working

--- index-compiled.js
+++ index-compiled.js
@@ -1,8 +1,16 @@
 /**
  * Created by Andrea on 2015.08.29..
  */
-
 "use strict";
+
+var markers = new Array();
+
+function clearMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = new Array();
+};
 
 google.maps.event.addDomListener(window, 'load', initializeMap);
 $(document).ready(function () {
@@ -53,26 +61,11 @@ $(document).ready(function () {
             data: townListFromJson.map(function (city, index) {
                 return { id: index, text: city };
             }),
-
             formatLoadMore: 'Loading more...'
         });
     });
 });
 
-/*query: function (q) {
-    // pageSize is number of results to show in dropdown
-    var pageSize,
-        results;
-    pageSize = 20;
-    results = this.data.filter(function (e) {
-        return (q.term === "" || e.text.toUpperCase().indexOf(q.term.toUpperCase()) === 0);
-    });
-    q.callback({
-        results: results.slice((q.page - 1) * pageSize, q.page * pageSize),
-        // retrieve more when user hits bottom
-        more: results.length >= q.page * pageSize
-    });
-}*/
 function initializeMap() {
     var myCenter = new google.maps.LatLng(47.456589, 18.928657);
     var mapProp = {
@@ -87,6 +80,7 @@ function initializeMap() {
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(i.lat, i.lng)
             });
+            markers.push(marker);
             marker.setMap(map);
 
             var contentString = "<div class=\"mapInfoWindow\">\n                <a href=\"" + i.detailsPageUrl + "\"><h1>" + i.mostSpecificLocation + "</h1></a>\n                <div><p><a href=\"" + i.detailsPageUrl + "\">\n                <img class=\"houseThumbnailInMapInfoWindow\" src=\"" + i.images[0] + "\" alt=\"Img\"></a></p>\n                <p>" + i.price + "</p>\n                </div>\n                </div>";
@@ -98,6 +92,62 @@ function initializeMap() {
             google.maps.event.addListener(marker, 'click', function () {
                 infowindow.open(map, marker);
             });
+        });
+    });
+}
+
+function refreshListAndMap() {
+    clearMarkers();
+    var typeSelector = document.getElementById("selectEstateType");
+    var selectedType = typeSelector.value;
+    console.log(typeSelector);
+    console.log(selectedType);
+    var selections = $("#town-input-1").select2('data');
+    var selectedTowns = [];
+    var saleCheckbox = document.getElementById('checkbox_sale');
+    var rentCheckbox = document.getElementById('checkbox_rent');
+    selections.forEach(function (i) {
+        selectedTowns.push(i.text);
+    });
+    console.log(selectedTowns);
+    var myCenter = new google.maps.LatLng(47.456589, 18.928657);
+    var mapProp = {
+        center: myCenter,
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var housesList = document.getElementById("houses-list");
+    housesList.innerHTML = "";
+    $.getJSON("houses.json", function (json) {
+        json.forEach(function (i) {
+            if ((selectedTowns.length === 0 || selectedTowns.filter(function (n) {
+                return i.locationTags.indexOf(n) >= 0;
+            }).length > 0) && (i.type === selectedType || selectedType === "bármilyen ingatlan") && (saleCheckbox.checked && i.forSaleOrRent === "sale" || rentCheckbox.checked && i.forSaleOrRent === "rent")) {
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(i.lat, i.lng)
+                });
+                markers.push(marker);
+                marker.setMap(map);
+
+                var contentString = "<div class=\"mapInfoWindow\">\n                <a href=\"" + i.detailsPageUrl + "\"><h1>" + i.mostSpecificLocation + "</h1></a>\n                <div><p><a href=\"" + i.detailsPageUrl + "\">\n                <img class=\"houseThumbnailInMapInfoWindow\" src=\"" + i.images[0] + "\" alt=\"Img\"></a></p>\n                <p>" + i.price + "</p>\n                </div>\n                </div>";
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                google.maps.event.addListener(marker, 'click', function () {
+                    infowindow.open(map, marker);
+                });
+
+                var li = document.createElement("li");
+                var roomsHtmlPart = "";
+                if (i.numberOfRooms !== undefined) {
+                    roomsHtmlPart = "<p><b>Szobák: </b>" + i.numberOfRooms + "</p>";
+                }
+                li.innerHTML = "<div class=\"frame\">\n                <a href=\"" + i.detailsPageUrl + "\">\n                <img class=\"houseThumbnailInFrontpageList\" src=\"" + i.images[0] + "\" alt=\"Img\" width=\"418px\" height=\"264px\"></a>\n                </div>\n                <h5>" + i.title + "</h5>\n                <div class=\"details\">\n                <p><b>Alapterület: </b>" + i.sizeInSquareMeters + " m<sup>2</sup></p>" + roomsHtmlPart + ("</div>\n                <span class=\"price\">" + i.price + "</span>");
+                housesList.appendChild(li);
+            }
         });
     });
 }
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /**
  * Created by Andrea on 2015.08.29..
  */
+var markers = new Array();
+
+function clearMarkers() {
+    for (var i=0; i<markers.length; i++){
+        markers[i].setMap(null);
+    }
+    markers = new Array();
+};
+
 
 google.maps.event.addDomListener(window, 'load', initializeMap);
 $(document).ready(function () {
@@ -62,22 +71,7 @@ $(document).ready(function () {
             data: townListFromJson.map(function (city, index) {
                 return {id: index, text: city};
             }),
-
             formatLoadMore: 'Loading more...',
-            /*query: function (q) {
-                // pageSize is number of results to show in dropdown
-                var pageSize,
-                    results;
-                pageSize = 20;
-                results = this.data.filter(function (e) {
-                    return (q.term === "" || e.text.toUpperCase().indexOf(q.term.toUpperCase()) === 0);
-                });
-                q.callback({
-                    results: results.slice((q.page - 1) * pageSize, q.page * pageSize),
-                    // retrieve more when user hits bottom
-                    more: results.length >= q.page * pageSize
-                });
-            }*/
         });
 
     });
@@ -98,6 +92,7 @@ function initializeMap() {
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(i.lat, i.lng)
             });
+            markers.push(marker);
             marker.setMap(map);
 
             var contentString =
@@ -116,6 +111,86 @@ function initializeMap() {
             google.maps.event.addListener(marker, 'click', function () {
                 infowindow.open(map, marker);
             });
+
+        });
+    });
+}
+
+function refreshListAndMap(){
+    clearMarkers();
+    var typeSelector = document.getElementById("selectEstateType");
+    var selectedType = typeSelector.value;
+    console.log(typeSelector);
+    console.log(selectedType);
+    var selections = $("#town-input-1").select2('data');
+    var selectedTowns = [];
+    var saleCheckbox = document.getElementById('checkbox_sale');
+    var rentCheckbox = document.getElementById('checkbox_rent');
+    selections.forEach(i => {
+        selectedTowns.push(i.text);
+    });
+    console.log(selectedTowns);
+    var myCenter = new google.maps.LatLng(47.456589, 18.928657);
+    var mapProp = {
+        center: myCenter,
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var housesList = document.getElementById("houses-list");
+    housesList.innerHTML = "";
+    $.getJSON("houses.json", function (json) {
+        json.forEach(i => {
+            if ( ((selectedTowns.length === 0) || ((selectedTowns.filter(function(n) {
+                    return (i.locationTags).indexOf(n) >= 0
+                }).length > 0)))
+            &&
+                ((i.type === selectedType) || (selectedType === "bármilyen ingatlan"))
+            && ((saleCheckbox.checked && i.forSaleOrRent === "sale") || (rentCheckbox.checked && i.forSaleOrRent === "rent"))
+            ) {
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(i.lat, i.lng)
+                });
+                markers.push(marker);
+                marker.setMap(map);
+
+                var contentString =
+                    `<div class="mapInfoWindow">
+                <a href="${i.detailsPageUrl}"><h1>${i.mostSpecificLocation}</h1></a>
+                <div><p><a href="${i.detailsPageUrl}">
+                <img class="houseThumbnailInMapInfoWindow" src="${i.images[0]}" alt="Img"></a></p>
+                <p>${i.price}</p>
+                </div>
+                </div>`;
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                google.maps.event.addListener(marker, 'click', function () {
+                    infowindow.open(map, marker);
+                });
+
+
+                var li = document.createElement("li");
+                var roomsHtmlPart="";
+                if (i.numberOfRooms !== undefined){
+                    roomsHtmlPart =  "<p><b>Szobák: </b>" + i.numberOfRooms + "</p>";
+                }
+                li.innerHTML = (
+                    `<div class="frame">
+                <a href="${i.detailsPageUrl}">
+                <img class="houseThumbnailInFrontpageList" src="${i.images[0]}" alt="Img" width="418px" height="264px"></a>
+                </div>
+                <h5>${i.title}</h5>
+                <div class="details">
+                <p><b>Alapterület: </b>${i.sizeInSquareMeters} m<sup>2</sup></p>`
+                    + roomsHtmlPart +
+                    `</div>
+                <span class="price">${i.price}</span>`
+                );
+                housesList.appendChild(li);
+            }
 
         });
     });
